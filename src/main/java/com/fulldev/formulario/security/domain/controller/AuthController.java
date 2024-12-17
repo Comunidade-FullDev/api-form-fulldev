98dff148318330083d0934d725393ed395d37e83
Eric <ericsouza94895@gmail.com>
2024-12-16 23:59:27 -0300

    feat: adicionando uma nova condição para verificar se existe um usuário com aquele email quando tenta fazer login

--- src/main/java/com/fulldev/formulario/security/domain/controller/AuthController.java
+++ src/main/java/com/fulldev/formulario/security/domain/controller/AuthController.java
@@ -22,6 +22,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("auth")
@@ -45,15 +46,17 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthDTO authDTO) {
         try {
+
+            if (authDTO.email() == null || !userService.passwordisValid(authDTO.password()))
+                return ResponseEntity.status(403).body("Não foi possível realizar o login do usuário. Existe algum campo obrigatório nulo, vazio ou inválido.");
+
             User user = (User) userRepository.findByEmail(authDTO.email());
 
             if (user == null)
-                return ResponseEntity.status(403).body("Não foi possível realizar o login do usuário. Existe algum campo obrigatório nulo ou vazio.");
-
+                return ResponseEntity.status(403).body("Não existe nehuma conta com esse endereço de email. faça o cadastro e depois volte para o login.");
 
             if (!user.isVerified())
-                return ResponseEntity.status(403).body("Email do usuário não verificado.");
-
+                return ResponseEntity.status(403).body("Email do usuário não foi verificado. Verifique-o e tente fazer login novamente");
 
             var emailAndPassword = new UsernamePasswordAuthenticationToken(authDTO.email(), authDTO.password());
             var auth = this.authenticationManager.authenticate(emailAndPassword);
@@ -71,10 +74,10 @@ public class AuthController {
     public ResponseEntity register(@RequestBody @Valid RegisterDTO registerDTO) {
         try {
             if (this.userRepository.findByEmail(registerDTO.email()) != null)
-                return ResponseEntity.badRequest().body("Usuário já existe");
+                return ResponseEntity.badRequest().body("Esse email já está em uso.");
 
             if (registerDTO.email() == null || !userService.passwordisValid(registerDTO.password()))
-                return ResponseEntity.status(403).body("Não foi possível realizar o registro do usuário. Existe algum campo obrigatório nulo ou vazio.");
+                return ResponseEntity.status(403).body("Não foi possível realizar o registro do usuário. Existe algum campo obrigatório nulo, vazio ou inválido.");
 
             String encryptedPassword = new BCryptPasswordEncoder().encode(registerDTO.password());
 
@@ -83,7 +86,7 @@ public class AuthController {
             String verificationToken = java.util.UUID.randomUUID().toString();
             user.setVerificationToken(verificationToken);
 
-            String verificationLink = "http://localhost:8080/auth/verify?token=" + verificationToken;
+            String verificationLink = "https://fulldev-seven.vercel.app/verify?token=" + verificationToken;
             emailService.sendVerificationEmail(user.getUsername(), "Confirmação de Cadastro", verificationLink);
 
             this.userRepository.save(user);
@@ -111,7 +114,59 @@ public class AuthController {
         return ResponseEntity.ok("Conta verificada com sucesso.");
     }
 
-    @DeleteMapping(path = {"/delete"+"/{id}"})
+    @PostMapping("/reset-password")
+    public ResponseEntity<String> requestPasswordReset(@RequestBody Map<String, String> payload) {
+        String email = payload.get("email");
+        if (email == null || email.isEmpty())
+            return ResponseEntity.badRequest().body("O campo email é obrigatório.");
+
+        if (userRepository.findByEmail(email) == null)
+            return ResponseEntity.badRequest().body("Não existe um usuário cadastrado com este e-mail.");
+
+        User user = (User) userRepository.findByEmail(email);
+
+        String resetToken = UUID.randomUUID().toString();
+        user.setResetToken(resetToken);
+
+        String resetLink = "https://fulldev-seven.vercel.app/reset-password?token=" + resetToken;
+        emailService.sendPasswordResetEmail(user.getEmail(), "Redefinição de Senha", resetLink);
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok("Instruções para redefinir sua senha foram enviadas ao seu e-mail.");
+    }
+
+    
+
+
+
+    @PostMapping("/reset-password/confirm")
+    public ResponseEntity confirmPasswordReset(@RequestBody Map<String, String> request) {
+        String token = request.get("token");
+        String newPassword = request.get("password");
+
+        User user = userRepository.findByResetToken(token);
+
+        if (user == null) {
+            return ResponseEntity.badRequest().body("Token inválido ou expirado.");
+        }
+
+        if (!userService.passwordisValid(newPassword)) {
+            return ResponseEntity.badRequest().body("A senha não atende aos critérios de segurança.");
+        }
+
+        String encryptedPassword = new BCryptPasswordEncoder().encode(newPassword);
+        user.setPassword(encryptedPassword);
+        user.setResetToken(null);
+
+        userRepository.save(user);
+
+        return ResponseEntity.ok("Senha redefinida com sucesso.");
+    }
+
+
+
+    @DeleteMapping(path = {"/delete/{id}"})
     public ResponseEntity delete(@PathVariable String id){
         return userRepository.findById(id)
                 .map(record -> {userRepository.deleteById(id);
